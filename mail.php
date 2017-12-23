257ff673517eebfb0a9e3bb355f3ec74b77cdd10
hankwing <hankwing@hotmail.com>
2017-12-23 15:06:37 +0800

    change something

--- mail.php
+++ mail.php
@@ -5,7 +5,6 @@
 	$number_of_gustes = $_POST['guest'];
 	$events = $_POST['events'];
 	$notes = $_POST['notes'];
-    echo "我的第一段 PHP 脚本！";
 	$subject = "Form submission";
 	$message = $sender_name . " is attending! The number of gustes of his / her is : " .  $number_of_gustes . " and his / her selected event is " . $events . ". He / she worte the following... ". "\n\n" . $notes;
 
